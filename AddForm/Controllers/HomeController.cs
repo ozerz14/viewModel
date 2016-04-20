f0e852eb8f199014526af492ba0c615c4c9db928
zehra <ozerz@itu.edu.tr>
2016-04-20 22:51:39 +0300

    viewModel initial commit.

---
+++ AddForm/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using AddForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AddForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        List<Student> students = new List<Student>();
+
+        public ActionResult Index()
+        {
+            var std = new Student();
+            std.Name = "ali";
+            std.Age = 22;
+
+            students.Add(std);
+
+            return View(students);
+        }
+
+        
+        public ActionResult AddObject() {
+            ViewModelStudent student = new ViewModelStudent();
+            return View(student);
+        }
+
+
+        [HttpPost]
+        public ActionResult AddObject(ViewModelStudent student) 
+        {
+            if (ModelState.IsValid)
+            {
+                Student tst = new Student
+                {
+                    Name = student.Name,
+                    Age = student.Fail ? 1 : 0
+                };
+                students.Add(tst);
+                return View("Index",students);
+            }
+
+            
+            return RedirectToAction("Index", "Home");
+
+        }
+
+    }
+}
---
+++ AddForm/Models/ViewModelStudent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddForm.Models
+{
+    public class ViewModelStudent
+    {
+        public string Name { get; set; }
+        public bool Fail { get; set; }
+    }
+}
